65ef2dee09f5a566709fb380d448db212d621bb9
Alexander Kostarev <UniverAlex@ya.ru>
2017-11-23 21:47:23 +0300

    Remove count in 2nd exercise

--- 2.py
+++ 2.py
@@ -1,14 +1,12 @@
-'''
+"""
 Задание 2.
 
 Спросить у пользователя число n.
 Напечатать числа от 1 до n. По 8 штук в строке.
-'''
+"""
 number = int(input('Enter a number: '))
-count = 1
-while count <= number:
-    print(count, end = " ")
-    if count % 8 == 0:
+for i in range(number):
+    print(i, end = " ")
+    if i % 8 == 0:
         print('\n')
-    count += 1
 print('\n')
