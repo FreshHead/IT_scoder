612cd147f5744871ca79ebcfa47a886e3f08a03b
Alexander Kostarev <UniverAlex@ya.ru>
2017-11-23 21:29:04 +0300

    Remove variable "count".

--- 1.py
+++ 1.py
@@ -1,12 +1,9 @@
-'''
+"""
 Задание 1.
 
 Спросить у пользователя число n.
-Напечатать числа от 1 до n. Через пробел.
-'''
+Напечатать числа от 1 до n. Через пробел. Сокращаем код убрав переменную count
+"""
 number = int(input('Enter a number: '))
-count = 1
-while count <= number:
-    print(count)
-    count += 1
-   
+for i in range(1, number + 1):
+    print(i)
