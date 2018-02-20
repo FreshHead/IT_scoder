be1aee76cd90d1e29094a757934cd222835c2c95
Alexander Kostarev <UniverAlex@ya.ru>
2018-02-20 14:16:43 +0300

    If applied this commit will add doctest for 5.py

--- 5.py
+++ 5.py
@@ -6,10 +6,24 @@
 
 
 def factorial(number):
+    """
+    >>> factorial(1)
+    1
+    >>> factorial(5)
+    120
+    >>> factorial(6)
+    720
+    """
+
     if number > 1:
         return number * factorial(number - 1)
     return 1
 
 
-number = int(input('Enter a number: '))
-print(factorial(input))
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+
+user_input = int(input('Enter a number: '))
+print(factorial(user_input))
