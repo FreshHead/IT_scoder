0b31192190dc14eb14e96cfe77ce817d602ed79a
Alexander Kostarev <UniverAlex@ya.ru>
2018-04-09 11:01:43 +0300

    If applied this will add solution on first exercise from "good habits for great coding" book

---
+++ good_habits_for_great_coding/1.py
@@ -0,0 +1,23 @@
+# optimize body of for loop with minimum multiplication
+def exercise():
+    total = 0
+    for n in range(1, 3000000):
+        total += (2 * n * n * n + 3 * n * n + 4 * n)
+    print('total =', total)
+
+
+def solution():
+    """
+    >>> solution()
+    total = 40500000000008999995500000
+    """
+    total = 0
+    for n in range(1, 3000000):
+        total += n * (2 * n * n + 3 * n + 4)
+    print('total =', total)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
