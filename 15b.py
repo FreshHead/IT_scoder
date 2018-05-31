54ddb6a563c4935de1f120000777a96dd78f2218
Alexander Kostarev <UniverAlex@ya.ru>
2018-05-31 09:03:41 +0300

    if applied this commit will add solution for 15

--- 15b.py
+++ 15b.py
@@ -5,18 +5,43 @@
 10 * 2 + 4 - 5 * 2 => 10 * 2 , 4, -5 * 2
 2. Заменяем одночлены их решением, если они таковым ещё не являются:
 10 * 2 => 20 , 4 => 4, -5 * 2 => -10
-3. Выполняем действия из массива операторов над одночленами,
-пока не получим окончательное решение:
+3. суммируем одночлены пока не получим окончательное решение:
 20 + 4 => 24, 24 + -10 => 14
 """
+import re
 
 
-# def calculate(input_string):
-#     assert isinstance(input_string, str), "input must be string!"
-#     if input_string.isdigit():
-#         return float(input_string)
-#     # split expression by multiplications and divisions
-#     operand = re.split('[*/]', input_string, 1)
-#
-#
-# print(calculate(input('Type a expression:\n')))
+def evaluate_simple(first_operand, operator, second_operand):
+    if operator == '*':
+        return float(first_operand) * float(second_operand)
+    elif operator == '/':
+        return float(first_operand) / float(second_operand)
+
+
+def evaluate_monomial(monomial):
+    if monomial.isdigit():
+        return monomial
+    operand1 = re.split('[*/]', monomial, 1)[0]
+    print(operand1)
+    operator = monomial[len(operand1)]
+    print(operator)
+    operand2 = monomial[len(operand1) + 1:]
+    print(operand2)
+    return evaluate_simple(operand1, operator, evaluate_monomial(operand2))
+
+
+def split_by_monomials(expression):
+    prepared_expression = expression.replace('-', '+-')
+    print(prepared_expression)
+    split_monomials = prepared_expression.split('+')
+    print(split_monomials)
+    total = 0
+    for monomial in split_monomials:
+        total += float(evaluate_monomial(monomial))
+        pass
+    print(total)
+
+
+test_expression = '10*2+4-5*2/4'
+
+split_by_monomials(test_expression)
